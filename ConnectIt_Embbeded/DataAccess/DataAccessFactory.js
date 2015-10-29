9cec4508b936b627b8d191c9cb21fe2f17217de0
anthonydasse <anthony.dasse@gmail.com>
2015-10-29 14:38:48 -0700

    Suppression des fichiers inutiles, suppression des dependances sqlite, mise en place de docker

--- ConnectIt_Embbeded/DataAccess/DataAccessFactory.js
+++ ConnectIt_Embbeded/DataAccess/DataAccessFactory.js
@@ -1,25 +1,11 @@
 var testBdd = {};
-var sqlite3 = require('sqlite3').verbose();
 exports.testBdd = testBdd;
-var fs = require("fs");
-var file = "connectIt.db";
+
 
 testBdd.insertTest = function(stringTest){
-  var db = new sqlite3.Database(file);
-  db.serialize(function() {
-    var stmt = db.prepare("INSERT INTO Test VALUES (?)");
-    stmt.run(stringTest);
-    stmt.finalize();
-  });
-  db.close();
+
 };
 
 testBdd.getAllFromTest = function(){
-  var db = new sqlite3.Database(file);
-  db.serialize(function() {
-    db.each("SELECT * from Test", function(err, row) {
-      console.log(row.id + ": " + row.Name);
-    });
-  });
-  db.close();
+
 };
---
+++ ConnectIt_Embbeded/Readme.txt
@@ -0,0 +1,14 @@
+Hello Bad boy :)
+
+Le projet embarque un fichier Dockerfile afin construire un container embarquant
+notre application NodeJS.
+
+Pré-requis :
+Il faut installer Docker sur votre poste ;)
+
+Construction du container :
+1 * vous placer au même niveau que le fichier DockerFile
+2 * Lancer : sudo docker build -t connectit-embbeded .
+
+Lancement de l'application :
+1 * sudo docker run connectit-embbeded
