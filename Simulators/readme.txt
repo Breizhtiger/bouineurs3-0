3c45db5cc2f9e7a1ba0b3026803e53762226edbf
anthonydasse <anthony.dasse@gmail.com>
2015-11-07 07:25:51 -0800

    init simulators project

---
+++ Simulators/readme.txt
@@ -0,0 +1,2 @@
+// This project contains all simulators to help during developments
+// Simulators : GPS, temperatures
---
+++ Simulators/simulator.js
@@ -0,0 +1,11 @@
+console.log("Simulator Started ...");
+//todo : init requirements, variables ...
+
+console.log("Clean all moq files...");
+//todo : clean all files
+
+console.log("GPS Simulator Started on the files /truc/test");
+//todo : launch GPS simulator
+
+console.log("Temp Sensor Simulator Started on the files /truc/test/temp1");
+//todo : launch Temp Sensor 1 simulator
