d6912ad38c34e048a56c4f1f888f9329602af428
Korrigane <lc.leroux@gmail.com>
2015-12-20 23:32:52 +0100

    Dernières modifs + Tentative connection Mongo

--- ConnectIt_Web/bouinappTest/app.js
+++ ConnectIt_Web/bouinappTest/app.js
@@ -5,12 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
+ 
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -56,9 +56,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
-
-
-
 
 module.exports = app;
