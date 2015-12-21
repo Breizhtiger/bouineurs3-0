ae9612f601604b4d5c6fc1896cbb7c48af0df75b
Korrigane <lc.leroux@gmail.com>
2015-12-21 22:32:34 +0100

    Tests Récup Infos DBMongo

--- ConnectIt_Web/bouinappTest/routes/users.js
+++ ConnectIt_Web/bouinappTest/routes/users.js
@@ -1,4 +1,6 @@
 var express = require('express');
+// Inclusion de Mongoose
+var mongoose = require('mongoose');
 var router = express.Router();
 
 /* GET users listing. */
@@ -20,11 +22,60 @@ router.get('/positions', function(req, res, next) {
   ]);
 });
 
+
+
+// On se connecte à la base de données
+// N'oubliez pas de lancer ~/mongodb/bin/mongod dans un terminal !
+mongoose.connect('mongodb://54.93.89.27/test', function(err) {
+  if (err) { throw err; }
+});
+ 
+// Création du schéma pour les commentaires
+// var commentaireArticleSchema = new mongoose.Schema({
+  // pseudo : { type : String, match: /^[a-zA-Z0-9-_]+$/ },
+  // contenu : String,
+  // date : { type : Date, default : Date.now }
+// });
+ 
+// Création du Model pour les commentaires
+// var CommentaireArticleModel = mongoose.model('commentaires', commentaireArticleSchema);
+ 
+ 
 /* GET users listing. */
-router.get('/messages', function(req, res, next) {
+// router.get('/messages', function(req, res, next) {
+  // //ici ton code qui récupère de la bdd
+ // // var message = 'un truc venant de la BDD, l heure (en timestamp): '+Date.now();
+  // var query = CommentaireArticleModel.find(null); 
+	// query.exec(function (err, comms) {
+	  // if (err) { throw err; }
+	
+  // res.send(comms[0].contenu);
+	// });
+// });
+
+/* GET images properties */
+// Création du schéma pour les images
+var imagePropertiesShema = new mongoose.Schema({
+  name : String,//, match: /^[a-zA-Z0-9-_]+$/ },
+  path : String,
+  GPS_coordinates : String,
+  speed : String,
+  date : { type : Date, default : Date.now  },
+  heure : String
+});
+ 
+// Création du Model pour les images
+var imagePropertiesModel = mongoose.model('image', imagePropertiesShema);
+ 
+ 
+router.get('/images', function(req, res, next) {
   //ici ton code qui récupère de la bdd
-  var message = 'un truc venant de la BDD, l heure (en timestamp): '+Date.now();
-  res.send(message);
+ // var message = 'un truc venant de la BDD, l heure (en timestamp): '+Date.now();
+  var query = imagePropertiesModel.find(null); 
+	query.exec(function (err, images) {
+		if (err) { throw err; }
+		res.send(images[0].path + images[0].name);
+	});
 });
 
 module.exports = router;
