b4e0fb430ecab64d0fd227738a486f207cb0b3a1
Korrigane <lc.leroux@gmail.com>
2015-12-31 00:31:33 +0100

    Modifs rétrospective

--- ConnectIt_Web/bouinappTest/public/js/grayscale.js
+++ ConnectIt_Web/bouinappTest/public/js/grayscale.js
@@ -44,7 +44,8 @@ function init() {
     zoom: 4
   });
 
-  $.get( "http://localhost:80/users/positions", function( data ) {
+  //http://localhost:80/users/positions
+  $.get( "http://www.bouineurs3-0.fr/users/positions", function( data ) {
       for(var i=0;i < data.length;i++){
         var marker = new google.maps.Marker({
           map: map,
