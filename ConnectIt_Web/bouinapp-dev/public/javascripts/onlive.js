6869eb26e013069eee1c1230cba80d195475a1bb
kbrault <contact@braultkevin.fr>
2016-02-04 22:00:58 +0100

    Test google map API

--- ConnectIt_Web/bouinapp-dev/public/javascripts/onlive.js
+++ ConnectIt_Web/bouinapp-dev/public/javascripts/onlive.js
@@ -1,5 +1,5 @@
-//
 
+/*
 var day = DAY;
 
 
@@ -16,5 +16,27 @@ var location = {
 
 $.get("http://localhost:3000/api/locations/daily/"+DAY){
 	[{location}, {location}];
-}
+};
+*/
 
+var locations = [
+    [32.681691, -4.735743],
+    [32.451755, -4.490545],
+    [32.235401, -4.631820]
+  ];
+
+function initMap() {
+  mapDay = new google.maps.Map(document.getElementById('mapDay'), {
+    zoom: 8,
+    center: new google.maps.LatLng(32.451755, -4.490545)
+  });
+
+  var marker, i;
+
+  for (i = 0; i < locations.length; i++) {
+        marker = new google.maps.Marker({
+          position: new google.maps.LatLng(locations[i][0], locations[i][1]),
+          map: mapDay
+        });
+  }
+}
